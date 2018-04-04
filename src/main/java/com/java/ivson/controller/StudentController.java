fcedaaf7f192d39aa3f316350172fec6d6843a4e
Ivson Santos <santosivson@hotmail.com>
2018-04-03 22:45:23 -0300

    Mongo DB pesquisando por nome e id

--- src/main/java/com/java/ivson/controller/StudentController.java
+++ src/main/java/com/java/ivson/controller/StudentController.java
@@ -1,8 +1,10 @@
 package com.java.ivson.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,15 +19,25 @@ public class StudentController {
 	@Autowired
 	StudentRepository studentRepository;
 	
-	//@GetMapping(value="/students")
 	@RequestMapping(value = "/students", method=RequestMethod.GET)
 	public List<Student> listStudent() {
 		return studentRepository.findAll();
 	}
 	
-	//@PostMapping
 	@RequestMapping(value = "/student", method=RequestMethod.POST)
 	public Student save(@RequestBody Student student) {
 		return this.studentRepository.save(student);
 	}
+	
+	@RequestMapping(value = "/student/{id}", method=RequestMethod.GET)
+	public Student findById(@PathVariable String id) {
+		Optional<Student> student = studentRepository.findById(id);
+		return student.orElseThrow(() -> new IllegalArgumentException("Objeto nao encontrado"));
+	}
+	
+	@RequestMapping(value = "/student/{name}/name", method=RequestMethod.GET)
+	public List<Student> findByName(@PathVariable String name) {
+		return studentRepository.findByNameLikeIgnoreCase(name);
+	}
+	
 }
--- src/main/java/com/java/ivson/repository/StudentRepository.java
+++ src/main/java/com/java/ivson/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.java.ivson.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.java.ivson.domain.Student;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
 
+	public List<Student> findByNameLikeIgnoreCase(String name);
 }
