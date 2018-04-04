fc7404ba7cf9c0cb35d6b8ec9c1aab5d489df68b
Ivson Santos <santosivson@hotmail.com>
2018-04-03 21:37:30 -0300

    Criado o projeto apontando para o Mongo MLAB

---
+++ src/main/java/com/java/ivson/SbFirstspringdataApplication.java
@@ -0,0 +1,12 @@
+package com.java.ivson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFirstspringdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFirstspringdataApplication.class, args);
+	}
+}
